a58a99d7039bf1c11338934aef71b8214d45df2d
Abdeldjalil Fradj <77704014+Abdeldjalilfradj1@users.noreply.github.com>
2023-02-10 11:13:39 -0500

    Update test_CCoinBox.py

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -2,6 +2,20 @@ import unittest
 from CCoinBox import CCoinBox
 
 class Test_CCoinBox(unittest.TestCase):
-
+    
     def test_pass(self):
         pass
+    def test_ajouter_25c(self):
+        self.coinBox = CCoinBox()
+        self.coinBox.ajouter_25c()
+        self.assertFalse(self.coinbox.get_vente_permise())
+
+    def test_vente(self):
+        self.coinBox = CCoinBox()
+        self.coinBox.ajouter_25c()
+        self.coinBox.ajouter_25c()
+        self.coinBox.ajouter_25c()
+        self.coinBox.ajouter_25c()
+        self.coinBox.vente()
+        self.assertTrue(self.coinbox.get_vente_permise())
+

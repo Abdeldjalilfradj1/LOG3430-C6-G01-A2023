dd95af04df707f0bdbef95e92a90ddcacab79e59
MLamothe <maxime.lamothe@polymtl.ca>
2023-02-10 09:08:25 -0500

    premier test

--- src/test_CCoinBox.py
+++ src/test_CCoinBox.py
@@ -3,11 +3,5 @@ from CCoinBox import CCoinBox
 
 class Test_CCoinBox(unittest.TestCase):
 
-    def setUp(self):
-        self.cb = CCoinBox()
-
-    def test_add25(self):
-        self.cb.ajouter_25c()
-        self.assertEqual(1, self.cb.get_monnaie_courante())
-
-
+    def test_pass(self):
+        pass
